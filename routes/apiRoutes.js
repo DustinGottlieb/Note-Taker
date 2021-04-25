d634cdf40a127b110ed01e2cc68c9d180a1670f0
dustingottlieb <dustingottlieb@gmail.com>
2021-04-25 14:56:20 -0500

    fixed git ignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,4 @@
 const fs = require('fs');
-const express = require('express');
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const notesData = require('../db/db.json');
-
-
 
 module.exports = (app) => {
 
